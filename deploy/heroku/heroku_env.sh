af8e439064180121c9910916c8a9cd9da20f33da
Eimantas Stonys <eimantas.stonys@gmail.com>
2024-03-04 17:19:40 +0200

    Run Caddy as root (same as default_baserow_env.sh)

--- deploy/heroku/heroku_env.sh
+++ deploy/heroku/heroku_env.sh
@@ -31,5 +31,4 @@ export EMAIL_SMTP_PASSWORD=${MAILGUN_SMTP_PASSWORD:-mailhog}
 DOCKER_USER=$(whoami)
 export DOCKER_USER
 
-# We must run the caddy user as the docker user to prevent supervisord errors
-export BASEROW_CADDY_USER="${BASEROW_CADDY_USER:-$DOCKER_USER}"
+export BASEROW_CADDY_USER="${BASEROW_CADDY_USER:-root}"
